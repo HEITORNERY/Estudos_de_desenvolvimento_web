6533700d733d3c98903760b250203f6738354859
HEITORNERY <joseheitorneryviana@gmail.com>
2024-07-23 15:33:32 -0300

    Programando em PHP

---
+++ scripts_em_php/primeiro_codigo_em_php.php
@@ -0,0 +1,8 @@
+<?php
+// para criar variável use o $nomedavariável
+$var1 = 1 + 1;
+// para printar uma imagem no terminal use o echo
+echo "O valor da variável var1 é " . $var1;
+// para saltar uma linha use o \n
+echo "\nObrigado pela explicação!\n";
+// sempre termine um código com ;
